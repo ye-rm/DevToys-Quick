9ecede2a2951e368c7cc4f42190004e5664a858f
Ye Wenyu <yewenyu@bupt.edu.cn>
2025-11-20 16:51:32 +0800

    initial commit

---
+++ src/common/constants.js
@@ -0,0 +1,40 @@
+// src/common/constants.js
+export default {
+    TOOLS: [
+      {
+        name: 'Base64 编解码',
+        icon: '/assets/icons/base64.svg',
+        route: '/pages/base64'
+      },
+      // {
+      //   name: 'JSON 格式化',
+      //   icon: '/assets/icons/json.svg',
+      //   route: '/pages/json-formatter'
+      // },
+      // {
+      //   name: '正则测试器',
+      //   icon: '/assets/icons/regex.svg',
+      //   route: '/pages/regex-tester'
+      // },
+      // {
+      //   name: '密码生成器',
+      //   icon: '/assets/icons/password.svg',
+      //   route: '/pages/password-gen'
+      // },
+      // {
+      //   name: 'Markdown 预览',
+      //   icon: '/assets/icons/markdown.svg',
+      //   route: '/pages/markdown-preview'
+      // },
+      // {
+      //   name: '进制转换',
+      //   icon: '/assets/icons/numbase.svg',
+      //   route: '/pages/number-base'
+      // },
+      // {
+      //   name: "哈希生成",
+      //   icon: '/assets/icons/hash.svg',
+      //   route: '/pages/hash-gen'
+      // }
+    ]
+  };
